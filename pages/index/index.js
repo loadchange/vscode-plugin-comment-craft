c7fc8fcb78a1d8dc4c877de617fb5398111cb7d8
LoadChange <soocto@gmail.com>
2017-08-24 22:27:26 +0800

    feat: implement user authentication system

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,25 +2,55 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
+    data: {
+        isOpenRemind: 0,
+        currentstate: 0,
+        lastOpenTime: '',
+        userInfo: null
+    },
+    //事件处理函数
+    bindViewTap: function () {
+        wx.navigateTo({
+            url: '../logs/logs'
+        })
+    },
+    onLoad: function () {
+        wx.setNavigationBarTitle({
+            title: '京证'
+        })
+        var that = this
+        app.getUserInfo(function (userInfo) {
+            console.log(userInfo)
+            that.setData({
+                userInfo: userInfo
+            })
+        })
+        this.clientProbe();
+    },
+    clientProbe: function () {
+        var that = this
+        wx.request({
+            url: 'https://api.jinjingzheng.zhongchebaolian.com/enterbj/jsp/enterbj/addcartype.jsp',
+            header: {
+                'content-type': 'text/html'
+            },
+            success: function (res) {
+                if (res.statusCode === 200) {
+                    if (res.data.indexOf('排队人数过多') < 0) {
+                        that.setData({
+                            currentstate: 1
+                        })
+                    }
+                }
+                that.setData({
+                    lastOpenTime: '2017-08-24 20:21:35'
+                })
+            }
+        })
+    },
+    switchRemind: function () {
+        this.setData({
+            isOpenRemind: this.data.isOpenRemind ? 0 : 1
+        })
+    }
 })
