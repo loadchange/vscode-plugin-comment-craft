83383cecd86c5f7f8ec913e681d36e722144dad2
LoadChange <soocto@gmail.com>
2017-08-27 19:03:20 +0800

    chore: update dependencies to latest stable versions

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,19 +6,51 @@ Page({
         lastOpenTime: '',
         userInfo: null
     },
+    dateDiff: function (dateTimeStamp) {
+        let result = "";
+        var minute = 1000 * 60;
+        var hour = minute * 60;
+        var day = hour * 24;
+        var month = day * 30;
+        var year = month * 12;
+        var now = new Date().getTime();
+        var diffValue = now - dateTimeStamp;
+        var yearC = diffValue / year;
+        var monthC = diffValue / month;
+        var weekC = diffValue / (7 * day);
+        var dayC = diffValue / day;
+        var hourC = diffValue / hour;
+        var minC = diffValue / minute;
+        if (yearC >= 1) {
+            result = parseInt(yearC) + "年前";
+        } else if (monthC >= 1) {
+            result = parseInt(monthC) + "个月前";
+        } else if (weekC >= 1) {
+            result = parseInt(weekC) + "周前";
+        } else if (dayC >= 1) {
+            result = parseInt(dayC) + "天前";
+        } else if (hourC >= 1) {
+            result = parseInt(hourC) + "小时前";
+        } else if (minC >= 1) {
+            result = parseInt(minC) + "分钟前";
+        } else {
+            result = "刚刚";
+        }
+        return result;
+    },
     clientProbe: function () {
         var that = this
         wx.request({
             url: 'https://jingzheng.hstba.com/current-state.json',
             success: function (res) {
-                if (!res.success) {
+                if (!res.data.success) {
                     return
                 }
                 that.setData({
-                    currentstate: res.currentstate
+                    currentstate: res.data.currentstate
                 })
                 that.setData({
-                    lastOpenTime: res.lastOpenTime
+                    lastOpenTime: that.dateDiff(res.data.lastOpenTime)
                 })
             }
         })
@@ -29,21 +61,26 @@ Page({
             return
         }
         wx.request({
-            url: 'https://jingzheng.hstba.com/get-user-switch-state.json',
-            data: {
-                openid: openid
-            },
+            url: 'https://jingzheng.hstba.com/get-user-switch-state.json?openid=' + openid,
             success: function (res) {
-                if (res.success) {
+                if (res.data.success) {
                     that.setData({
-                        isOpenRemind: remind
+                        isOpenRemind: res.data.remind
                     })
                 }
             }
         })
     },
     syncUserInfo: function (openid) {
-        var userInfo = this.data.userInfo;
+        var userInfo = this.data.userInfo || {
+            avatarUrl: 'http://wx4.sinaimg.cn/mw690/abf9e936ly1fiygm6u588j208c08cdg0.jpg',
+            city: '',
+            country: '',
+            gender: '',
+            language: '',
+            nickName: '游客',
+            province: ''
+        };
         userInfo.openid = openid;
         this.setData({
             userInfo: userInfo
@@ -60,19 +97,13 @@ Page({
         wx.login({
             success: function (res) {
                 if (res.code) {
-                    console.log(res.code)
                     wx.request({
-                        url: 'https://jingzheng.hstba.com/login.json',
+                        url: 'https://jingzheng.hstba.com/login.json?code=' + res.code,
                         method: 'POST',
-                        data: {
-                            code: res.code
-                        },
                         success: function (loginRes) {
-                            that.syncUserInfo(loginRes.openid)
+                            that.syncUserInfo(loginRes.data.openid)
                         }
                     })
-                } else {
-                    console.log('获取用户登录态失败！' + res.errMsg)
                 }
             }
         });
@@ -87,7 +118,6 @@ Page({
     onLoad: function () {
         var that = this
         app.getUserInfo(function (userInfo) {
-            console.log(userInfo)
             that.setData({
                 userInfo: userInfo
             })
@@ -107,9 +137,8 @@ Page({
             openid: this.data.userInfo.openid
         }
         wx.request({
-            url: 'https://jingzheng.hstba.com/switch-remind.json',
-            method: 'POST',
-            data: data
+            url: 'https://jingzheng.hstba.com/switch-remind.json?state=' + data.state + '&formId=' + data.formId + '&openid=' + data.openid,
+            method: 'POST'
         })
     }
 })
