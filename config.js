dae191d0703341ca71b40c7be8a1e1db03e22604
LoadChange <soocto@gmail.com>
2022-05-29 10:35:50 +0800

    docs: add contributing guidelines to repository

---
+++ config.js
@@ -0,0 +1,2 @@
+export const nftmarketaddress = "0x9560a675bD51b52C6c0e8F3629A6E83cC38fB868"
+export const nftaddress = "0x78c8361A5ed01dF1971525ff88Db5cb8Adf5caFd"
--- src/components/Footer/constant.js
+++
@@ -1,7 +0,0 @@
-export const ICON_LINK_LIST = [
-  { href: '#', icon: 'icon-twitter' },
-  { href: '#', icon: 'icon-discord' },
-  { href: '#', icon: 'icon-OpenSea-fill' },
-  { href: '#', icon: 'icon-facebook' },
-  { href: '#', icon: 'icon-instagram' },
-];
